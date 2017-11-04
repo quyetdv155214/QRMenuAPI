d3497e3d9219c8570f20cec29669478227a45ba3
TEYUQ\Quyet <quyetdv155214@gmail.com>
2017-11-04 19:38:07 +0700

    update categoy

--- model/category.py
+++ model/category.py
@@ -6,8 +6,8 @@ from model.item import Item
 
 
 class Categoty(Document):
-    menu_id = StringField(max_length=20, min_length=3, required=True)
-    cate_id = StringField(max_length=20, min_length=3, unique=True, required=True)
+    menu_id = StringField(max_length=100, min_length=3, required=True)
+    cate_id = StringField(max_length=100, min_length=3, unique=True, required=True)
     cate_name = StringField(max_length=200, min_length=1, required=True)
     cate_type = StringField()
     cate_order = IntField(min_value=1, default=1)
--- model/menu.py
+++ model/menu.py
@@ -6,8 +6,8 @@ from model.item import Item
 
 
 class Menu(Document):
-    res_id = StringField(max_length=20, min_length=3, required=True)
-    menu_id = StringField(max_length=20, min_length=3, unique=True, required=True)
+    res_id = StringField(max_length=100, min_length=3, required=True)
+    menu_id = StringField(max_length=100, min_length=3, unique=True, required=True)
     menu_name = StringField()
     date_create = StringField(required=False)
     describe = StringField(max_length=500)
